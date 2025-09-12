bd077623d0461ed51d4939bfb5d27532769017dc
Paramdeep Singh <psingh20@rrc.ca>
2025-09-11 20:04:44 -0500

    added health check endpoints and tested works well

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-import express, { Express } from "express";
+import express, { Express,Request, Response } from "express";
 
 // Initialize Express application
 const app: Express = express();
 
 // Define a route
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+
+// Health check endpoint
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
 });
 
+
 export default app;
+
+
+
+
